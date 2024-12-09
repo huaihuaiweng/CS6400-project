d294bb79c7dfe1ab6933f8c7cf5a91f39e17da4d
ExtraneousFalcon <saianoop9@gmail.com>
2024-12-09 16:13:26 -0500

    cleaning up

--- app/Home.py
+++ app/Home.py
@@ -7,15 +7,12 @@ sys.path.append(project_root)# Import apps
 from app.Reading_List import app as reading_list_app
 from app.Recommendations import app as recommendations_app
 
-# Initialize session state for page navigation
 if "page" not in st.session_state:
     st.session_state.page = "Reading List"
 
-# Sidebar for Navigation
 st.sidebar.title("Navigation")
 st.session_state.page = st.sidebar.radio("Select a Page", ["Reading List", "Recommendation Page"])
 
-# Page Navigation
 if st.session_state.page == "Reading List":
     reading_list_app()
 elif st.session_state.page == "Recommendation Page":
